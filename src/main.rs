687aaf1a391df699ae27814a6e90c882eeb18e19
DeltaManiac <maxpaynered@gmail.com>
2019-02-06 23:33:23 +0530

    Added more structs for Manifest

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,74 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
-#[derive(Serialize, Deserialize,  Debug)]
-struct Instance {
+#[derive(Serialize, Deserialize, Debug)]
+struct Job {
     name: String,
-    azs: Vec<String>,
+    release: String,
+    // consumes:Option<String>
+    // provides:Option<String>
+    // properties:Option<String>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Features {
+struct Network {
+    name: String,
+    static_ips: Option<Vec<String>>,
+    default: Option<Vec<String>>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct VMResource {
+    cpu: i16,
+    ram: i16,
+    ephemeral_disk_size: i16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Instance {
+    name: String,
+    azs: Vec<String>,
+    instances: i16,
+    jobs: Vec<Job>,
+    vm_type: String,
+    vm_resources: Option<VMResource>,
+    stemcell: String,
+    persistent_disk: Option<i16>,
+    persistent_disk_type: Option<String>,
+    networks: Vec<Network>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Feature {
     converge_variable: Option<bool>,
     randomize_az_placement: Option<bool>,
     use_dns_address: Option<bool>,
     use_tmpfs_job_config: Option<bool>,
 }
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Stemcell {
-    os: String,
+    alias: String,
+    os: Option<String>,
     version: String,
+    name: Option<String>,
 }
-#[derive(Serialize, Deserialize,  Debug)]
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Release {
     name: String,
     version: String,
     url: Option<String>,
     sha1: Option<String>,
-    stemcell: Option<Stemcell>,
+    //stemcell: Option<Stemcell>,
 }
 
-#[derive(Serialize, Deserialize,  Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Manifest {
     instance_groups: Vec<Instance>,
     name: String,
-    features: Option<Features>,
+    features: Option<Feature>,
+    releases: Vec<Release>,
+    stemcells: Vec<Stemcell>,
 }
 
 fn load_yml() -> io::Result<()> {
