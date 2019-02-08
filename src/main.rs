9e4f7be56fcbd919c72b69a99136d975e2fff6c9
DeltaManiac <maxpaynered@gmail.com>
2019-02-08 14:22:50 +0530

    Initial rendering logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #[macro_use]
 extern crate serde_derive;
-use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::io;
-use std::io::prelude::*;
+use svg::node::element::*;
+use svg::Document;
+
+const RECT_WIDTH: u32 = 150;
+const RECT_HEIGHT: u32 = 200;
+const RECT_PADDING: u32 = 10;
+const TEXT_PADDING_LEFT: u32 = RECT_PADDING + 20;
+const TEXT_PADDING_TOP: u32 = RECT_PADDING + 20;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Job {
@@ -75,15 +80,67 @@ struct Manifest {
     stemcells: Vec<Stemcell>,
 }
 
-fn load_yml() -> io::Result<()> {
-    let mut f = File::open("sample/zookeeper.yml")?;
-    let c: Manifest = serde_yaml::from_reader(f).unwrap();
+fn load_yml() -> Manifest {
+    let f = File::open("sample/zookeeper.yml").expect("Couldnt open file");
+    let m: Manifest = serde_yaml::from_reader(f).unwrap();
+    m
+}
 
-    dbg!(&c);
+fn draw_svg(manifest: &Manifest) {
+    let background = Rectangle::new()
+        .set("width", "100%")
+        .set("height", "100%")
+        .set("fill", "black ");
 
-    Ok(())
+    let mut document = Document::new()
+        .set("manifest", (0, 0, 100, 70))
+        .add(background);
+
+    for (idx, instance) in (&manifest.instance_groups).iter().enumerate() {
+        let group = Group::new();
+        let instance_rect = Rectangle::new()
+            .set(
+                "x",
+                10 + RECT_WIDTH * (idx as u32) + RECT_PADDING * (idx as u32),
+            )
+            //.set("y", 10 + RECT_HEIGHT * (idx as u8) + RECT_PADDING)
+            .set("y", RECT_PADDING)
+            .set("width", RECT_WIDTH)
+            .set("height", RECT_HEIGHT)
+            .set("fill", "lightblue");
+
+        let instance_name_text = Text::new()
+            .set("fill", "yellow")
+            .set(
+                "x",
+                TEXT_PADDING_TOP + RECT_WIDTH * (idx as u32) + RECT_PADDING * (idx as u32),
+            )
+            .set("y", TEXT_PADDING_LEFT)
+            .add(svg::node::Text::new(instance.name.clone()));
+
+        let mut zones: String = "[ ".to_string();
+        for az in &instance.azs {
+            zones.push_str(&az[..]);
+            zones.push_str(" ");
+        }
+        zones.push_str("]");
+        let instance_azs_text = Text::new()
+            .set("fill", "yellow")
+            .set(
+                "x",
+                TEXT_PADDING_LEFT + RECT_WIDTH * (idx as u32) + RECT_PADDING * (idx as u32),
+            )
+            .set("y", TEXT_PADDING_TOP * 2)
+            .add(svg::node::Text::new(zones.clone()));
+        document = document
+            .add(group.add(instance_rect).add(instance_name_text))
+            .add(instance_azs_text);
+    }
+    svg::save("image.svg", &document).unwrap();
 }
 
 fn main() {
-    load_yml();
+    //load_yml();
+    let manifest = load_yml();
+    draw_svg(&manifest);
 }
